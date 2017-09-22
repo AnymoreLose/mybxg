a806f8ed9d62082a0de977c262e3c8bde8d8789c
hzy <1367116265@qq.com>
2017-09-22 21:51:19 +0800

    完成讲师查看功能

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -1,4 +1,4 @@
-define(['jquery','template'],function($,template) {
+define(['jquery','template','bootstrap'],function($,template) {
     // 调用接口 获取所有的讲师数据
     $.ajax({
         type : 'get',
@@ -39,6 +39,30 @@ define(['jquery','template'],function($,template) {
 
                 })
             });
+
+            // 查看讲师 功能
+            $('.preview').click(function() {
+
+                // 获取 按钮的父元素   获取 ID
+                var td = $(this).closest('td');
+                var tcId = td.attr('data-tcId');
+                $.ajax({
+                    type : 'get',
+                    url : '/api/teacher/view',
+                    data : {tc_id : tcId},
+                    dataType : 'json',
+                    success : function(data) {
+                        console.log(data);
+                        // 模板渲染
+                        var html = template('modalTpl',data.result);
+                        console.log(html);
+                        $('#modalInfo').html(html);
+                        $('#teacherModal').modal();
+                    }
+                })
+            });
+
+
         }
     });
 
