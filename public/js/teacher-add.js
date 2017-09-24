13d9143a131788b9dd5e4b3f90364e5484d83491
hzy <1367116265@qq.com>
2017-09-24 10:42:52 +0800

    完成讲师添加和编辑功能

--- public/js/teacher-add.js
+++ public/js/teacher-add.js
@@ -14,12 +14,38 @@ define(['jquery','template','util'],function($,template,util) {
                 // 解析数据 渲染页面
                 var html = template('teacherTpl',data.result);
                 $('#teacherInfo').html(html);
+
+                // 教师信息 修改
+                submitForm('/api/teacher/update');
+                //submitForm('update');
             }
         })
     }else {
         //不存在 添加操作
         var html = template('teacherTpl',{});
         $('#teacherInfo').html(html);
+
+        // 教师信息 添加
+        submitForm('/api/teacher/add');
+        //submitForm('add');
+    }
+
+    // 实现表单 修改、添加功能
+    function submitForm(url) {
+        $('#teacherBtn').click(function() {
+            $.ajax({
+                type: 'post',
+                //url : '/api/teacher/' + url,
+                url : url,
+                data : $('#teacherForm').serialize(),
+                dataType : 'json',
+                success : function(data) {
+                    if(data.code == 200) {
+                        location.href = '/teacher/list';
+                    }
+                }
+            })
+        });
     }
 
 });
