19b4f0a67f8f5eeec18cfd8a5447caa2a9e95a80
hzy <1367116265@qq.com>
2017-09-26 19:41:05 +0800

    完成二级分类联动操作

--- public/js/course-basic.js
+++ public/js/course-basic.js
@@ -1,4 +1,4 @@
-define(['jquery','template','util'],function($,template,util) {
+define(['jquery','template','util','ckeditor'],function($,template,util,CKEDITOR) {
 
     // 设置 导航菜单选中高亮显示
     util.setMenu('/course/add');
@@ -16,16 +16,47 @@ define(['jquery','template','util'],function($,template,util) {
         success : function(data) {
             console.log(data);
             //解析数据，渲染页面
-            if(flag) {
+            if (flag) {
                 data.result.operate = '课程编辑';
-            }else {
+            } else {
                 data.result.operate = '课程添加';
             }
 
-            var html = template('basicTpl',data.result);
+            var html = template('basicTpl', data.result);
             $('#basicInfo').html(html);
-        }
 
-    })
+            //处理 二级分类的下拉联动
+            $('#firstType').change(function () {
+                // 获取一级分类的ID
+                var pid = $(this).val();
+                // 根据一级分类的ID获取二级分类的数据
+                $.ajax({
+                    type: 'get',
+                    url: '/api/category/child',
+                    data: {cg_id: pid},
+                    dataType: 'json',
+                    success: function (data) {
+                        console.log(data);
+                        // 拼接二级分类的下拉数据
+                        var tpl = '<option value="">请选择二级分类</option>'
+                            + '{{each list}}'
+                            + '<option value="{{$value.cg_id}}">{{$value.cg_name}}</option>'
+                            + '{{/each}}';
+
+                        var html = template.render(tpl, {list: data.result});
+                        $('#secondType').html(html);
+                    }
+                })
+            });
+
+            // 处理富文本
+            CKEDITOR.replace('editor',{
+                toolbarGroups : [
+                    { name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+                    { name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] }
+                ]
+            });
+        }
+    });
 
 });
