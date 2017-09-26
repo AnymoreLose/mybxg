a593e70790ae60eb9f429c1e8a9db522c47e62cf
hzy <1367116265@qq.com>
2017-09-26 11:56:18 +0800

    bug

---
+++ public/js/course-add.js
@@ -0,0 +1,6 @@
+define(['jquery','template','util'],function($,template,util) {
+
+    // 设置 导航菜单选中高亮显示
+    util.setMenu(location.pathname);
+
+});
---
+++ public/js/course-list.js
@@ -0,0 +1,7 @@
+define(['jquery','template','util'],function($,template,util) {
+
+    // 设置 导航菜单选中高亮显示
+    util.setMenu(location.pathname);
+    console.log(location.pathname);
+
+});
