e8677385706354e51791210e596329929eeb2906
hzy <1367116265@qq.com>
2017-09-27 10:53:48 +0800

    完成封面上传功能

--- public/js/course-picture.js
+++ public/js/course-picture.js
@@ -1,6 +1,40 @@
-define(['jquery','template','util'],function($,template,util) {
+define(['jquery','template','util','uploadify'],function($,template,util) {
 
-    // ���õ����˵�ѡ��
+    // 设置导航菜单选中
     util.setMenu('/course/add');
+    // 获取课程ID
+    var csId = util.qs('cs_id');
+    // 获取课程封面数据
+    $.ajax({
+        type : 'get',
+        url : '/api/course/picture',
+        data : {cs_id : csId},
+        dataType : 'json',
+        success : function(data) {
+            console.log(data);
+            // 解析数据，渲染页面
+            var html = template('pictureTpl', data.result);
+            $('#pictureInfo').html(html);
+
+            // 处理封面上传
+            $('#myfile').uploadify({
+                width : 80,
+                height : 'auto',
+                itemTemplate : '<span></span>',
+                buttonText : '选择图片',
+                buttonClass : 'btn btn-success btn-sm',
+                swf : '/public/assets/uploadify/uploadify.swf',
+                uploader : '/api/uploader/cover',
+                fileObjName : 'cs_cover_original',
+                formData : {cs_id : csId},
+                onUploadSuccess : function(a,b,c) {
+                    //console.log(b)
+                    // 转换         trim()：去掉空格
+                    var obj = JSON.parse(b.trim());
+                    $('.preview img').attr('src',obj.result.path)
+                }
+            });
+        }
+    });
 
 });
